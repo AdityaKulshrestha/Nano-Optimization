8a8a81860b64b0d6b0f5c8c2664dfe37a0841418
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2026-05-29 21:14:15 +0000

    redundant code

--- Triton/01-vector_addition/benchmark.py
+++ Triton/01-vector_addition/benchmark.py
@@ -43,4 +43,3 @@ if __name__ == "__main__":
     # We can now run the decorated function above. Pass `print_data=True` to see the performance number, `show_plots=True` to plot them, and/or
     # `save_path='/path/to/results/' to save them to disk along with raw CSV data:
     benchmark.run(print_data=True, save_path='./results/')
-    triton.testing.run_benchmarks()
