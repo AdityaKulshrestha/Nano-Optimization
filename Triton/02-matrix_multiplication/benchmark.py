d2abcad876f24afbd8e9620f17970b55170b0862
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2026-05-30 21:42:47 +0000

    Added benchmark code

---
+++ Triton/02-matrix_multiplication/benchmark.py
@@ -0,0 +1,56 @@
+import torch
+
+import triton
+import triton.language as tl
+
+from main import matmul
+
+ref_lib = 'cuBLAS' if is_cuda() else 'rocBLAS'
+
+configs = []
+
+for fp8_inputs in [False, True]:
+    if fp8_inputs and (not TORCH_HAS_FP8 or not is_cuda()):
+        continue
+    
+    configs.append(
+        triton.testing.Benchmark(
+            x_names=["M", "N", "K"],  # Argument names to use as an x-axis for the plot
+            x_vals=[128 * i for i in range(2, 33)],     # Different possible values for `x_name`
+            line_args='provider',
+            line_vals=['triton'] if fp8_inputs else [ref_lib.lower(), 'triton'],  # Argument name to use for grouping the results with different lines
+            styles=[("green", "-"), ("blue", "-")],
+            ylabel="TFLOPS", 
+            plot_name="matmul-performance-" + 
+            ("fp16" if not fp8_inputs else "fp8"),
+            args={
+                "fp8_inputs": fp8_inputs,
+            }
+        )
+    )
+
+
+@triton.testing.perf_report(configs)
+def benchmark(M, N, K, provider, fp8_inputs):
+    a = torch.randn((M, K), device=DEVICE, dtype=torch.float16)
+    b = torch.randn((K, N), device=DEVICE, dtype=torch.float16)
+
+    quantiles = [0.5, 0.2, 0.8]
+    
+    if provider == ref_lib.lower():
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: torch.matmul(a, b), quantiles=quantiles)
+
+    if provider == "triton":
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: matmul(a, b), quantiles=quantiles)
+
+    perf = lambda ms: 2 * M * N * K * 1e-12 / (ms * 1e-3)
+
+    return perf(ms), perf(max_ms), perf(min_ms)
+
+
+
+
+if __name__ == "__main__":
+    # We can now run the decorated function above. Pass `print_data
+
+    benchmark.run(print_data=True, save_path='./results/')
