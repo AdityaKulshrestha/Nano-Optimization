74ec50fda90b3d6ecb272913c4d12437b078c882
AdityaKulshrestha <kulshresthaaditya02@gmail.com>
2026-05-30 21:44:01 +0000

    Fixed imports error

--- Triton/02-matrix_multiplication/benchmark.py
+++ Triton/02-matrix_multiplication/benchmark.py
@@ -3,7 +3,7 @@ import torch
 import triton
 import triton.language as tl
 
-from main import matmul, is_cuda, TORCH_HAS_FP8
+from main import matmul, is_cuda
 
 ref_lib = 'cuBLAS' if is_cuda() else 'rocBLAS'
 
